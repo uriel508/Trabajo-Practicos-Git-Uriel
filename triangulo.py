acfd075682dd741ae36eea5ea0857b1dfe87ab2b
Uriel Agüero Cuello <urielcaguero17@gmail.com>
2025-08-29 17:30:05 -0300

    Agregue clase triangulo

---
+++ triangulo.py
@@ -0,0 +1,21 @@
+class Triangulo:
+    def __init__(self, lado1, lado2, lado3):
+        self.lado1 = lado1
+        self.lado2 = lado2
+        self.lado3 = lado3
+
+    def perimetro(self):
+        return self.lado1 + self.lado2 + self.lado3
+
+    def area(self):
+        s = self.perimetro() / 2
+        return (s * (s - self.lado1) * (s - self.lado2) * (s - self.lado3)) ** 0.5
+
+    def forma(self):
+        if self.lado1 == self.lado2 == self.lado3:
+            return "Equilátero"
+        elif self.lado1 == self.lado2 or self.lado2 == self.lado3 or self.lado1 == self.lado3:
+            return "Isósceles"
+        else:
+            return "Escaleno"
+
