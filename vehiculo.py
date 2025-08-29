54719cf20a0dfd11466c8eb9fc350007d8acc81a
Uriel Agüero Cuello <urielcaguero17@gmail.com>
2025-08-29 17:33:16 -0300

    Agregue clase vehiculo

---
+++ vehiculo.py
@@ -0,0 +1,6 @@
+class Vehiculo:
+    def __init__(self, marca, modelo, patente, color):
+        self.marca = marca
+        self.modelo = modelo
+        self.patente = patente
+        self.color = color
